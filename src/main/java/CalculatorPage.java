98edbfc000569cea6af4217a9ae81c39deb6555e
lizkagr <artur.demianenko@stud.techin.lt>
2025-06-20 14:02:34 +0600

    update logout rn work well

--- src/main/java/CalculatorPage.java
+++ src/main/java/CalculatorPage.java
@@ -2,6 +2,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class CalculatorPage {
     private WebDriver driver;
@@ -15,8 +18,10 @@ public class CalculatorPage {
     }
 
     public void logout() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(logoutButton));
         logoutButton.click();
     }
-
 }
+
 
